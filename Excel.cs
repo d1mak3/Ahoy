c82f30756994bac0e149ceee118586ef7877bc9a
d1mak3 <d1mak3@yandex.ru>
2020-10-12 01:03:45 +0300

    v 0.0.2 Изменил библиотеку. Убрал оттуда ненужные функции и добавил функцию очистки таблицы.

--- Excel.cs
+++ Excel.cs
@@ -1,17 +1,15 @@
-﻿// Thx Ukushu for library (Library has been taken from https://github.com/ukushu/DataExporter/blob/master/Excel.cs)
+﻿// Thx Ukushu for library source (Library has been taken from https://github.com/ukushu/DataExporter/blob/master/Excel.cs)
 
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using ClosedXML.Excel;
 
 namespace Test
 {
   public class Excel
   {
-    public List<List<string>> Rows = new List<List<string>>();
-
-    public void FileOpen(string path)
+    public List<List<string>> Rows = new List<List<string>>(); // Таблица из Excel в виде двумерного списка
+    
+    public void FileOpen(string path) // Открываем файл -> заполняем список
     {
 
       var workbook = new XLWorkbook(path);
@@ -20,7 +18,6 @@ namespace Test
       foreach (var xlRow in ws1.RangeUsed().Rows())
       {
         Rows.Add(new List<string>());
-
         foreach (var xlCell in xlRow.Cells())
         {
           var formula = xlCell.FormulaA1;
@@ -30,73 +27,12 @@ namespace Test
 
           Rows[Rows.Count - 1].Add(targetCellValue);
         }
-      }
+      }      
     }
 
-    public void FileSave(string path)
+    public void ClearFile() // Очищаем список
     {
-      CreateDirIfNotExist(path, true);
-
-      using (XLWorkbook wb = new XLWorkbook())
-      {
-        var workSheet = wb.Worksheets.Add("Sample Sheet");
-
-        for (int row = 0; row < Rows.Count; row++)
-        {
-          for (int col = 0; col < Rows[row].Count; col++)
-          {
-            var cellAdress = GetExcelPos(row, col);
-
-            if (Rows[row][col].StartsWith("="))
-            {
-              workSheet.Cell(cellAdress).FormulaA1 = Rows[row][col];
-            }
-            else
-            {
-              workSheet.Cell(cellAdress).Value = Rows[row][col];
-            }
-          }
-        }
-
-        wb.SaveAs(path);
-      }
-    }
-
-    public void AddRow(params string[] cells)
-    {
-      Rows.Add(cells.ToList());
-    }
-
-    public static string GetExcelPos(int row, int cell)
-    {
-      char[] alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
-
-      int count = cell / 26;
-      string alphResult = string.Empty;
-
-      if (count > 0)
-      {
-        alphResult = alph[count] + alph[count % 26].ToString();
-      }
-      else
-      {
-        alphResult = alph[cell].ToString();
-      }
-
-      return alphResult + (row + 1);
-    }
-
-    private void CreateDirIfNotExist(string dirPath, bool removeFilename = false)
-    {
-      if (removeFilename)
-      {
-        dirPath = Directory.GetParent(dirPath).FullName;
-      }
-
-      if (!Directory.Exists(dirPath))
-      {
-        Directory.CreateDirectory(dirPath);
-      }
+      Rows.Clear();
     }
   }
 }
