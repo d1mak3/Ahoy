cfa331ef4c65cd7c06d448e3fa4fa9b2553ac325
Администратор <@DESKTOP-PHORFLN>
2020-10-22 21:12:50 +0300

    v0.0.0 AskToDownload method added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,15 +28,25 @@ namespace Ahoy
 
             List<string> comporator = downloader.UnsortedLinks;
 
+            string path = @"Environment.CurrentDirectory" + @"\links.txt";
 
-            
-      MessageBoxResult result = MessageBox.Show("Wanna Download?", "Downloader", MessageBoxButton.YesNo, MessageBoxImage.Question);
-      bool checker = false;
-      if (result == MessageBoxResult.Yes)
-      {
-        checker = true;
-      }
-      return checker;
+            string[] readText = File.ReadAllLines(path);
+
+            bool checker = false;
+
+            for (int i = 0; i < readText.Length; i++)
+            {
+                if (comporator[i] != readText[i])
+                {
+                    MessageBoxResult result = MessageBox.Show("Wanna Download?", "Downloader", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        checker = true;
+                    }
+                }
+            }
+            return checker;
     }
 
     public MainWindow()
